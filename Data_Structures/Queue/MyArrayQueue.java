92177070f81fc2c8371b969d80dc172b0c7acbe8
neelbavarva <neelbavarva5@gmail.com>
2021-01-05 21:53:14 +0530

    queue with stack

---
+++ Data_Structures/Queue/MyArrayQueue.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class MyArrayQueue {
+    private int[] items;
+    private int rear;
+    private int front;
+    private int count;
+
+    public MyArrayQueue(int capacity) {
+        items = new int[capacity];
+    }
+
+	public void enqueue(int item) {
+        if(count == items.length){
+            throw new IllegalStateException();
+        }
+
+        items[rear] = item;
+        rear = (rear+1)%items.length;
+        count++;
+    }
+
+    public int dequeue() {
+        var item = items[front];
+        items[front] = 0;
+        front = (front+1)%items.length;
+        count--;
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+}
---
+++ Data_Structures/Queue/QueueWithStack.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class QueueWithStack {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public void enqueue(int item){
+        stack1.push(item);
+    }
+
+    public int dequeue() {
+        if(stack1.isEmpty() && stack2.isEmpty()){
+            throw new IllegalStateException();
+        }
+
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if(stack1.isEmpty() && stack2.isEmpty()){
+            throw new IllegalStateException();
+        }
+
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+}
