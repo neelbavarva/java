e4da2dfbb4ef55e5018961fccc8a875c2fe2e6a8
neelbavarva <neelbavarva5@gmail.com>
2021-02-21 18:02:37 +0530

    array

---
+++ Data_Structures/Arrays/JuggleArray.java
@@ -0,0 +1,37 @@
+// Rotating an Array 
+
+public class JuggleArray {
+
+    public static int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+
+    public static void factorial(int arr[], int d) {
+        int n = arr.length;
+        d = d % n;
+        int i, j, k, temp;
+        int g_c_d = gcd(d, n);
+        for (i = 0; i < g_c_d; i++) {
+            /* move i-th values of blocks */
+            temp = arr[i];
+            j = i;
+            while (true) {
+                k = j + d;
+                if (k >= n)
+                    k = k - n;
+                if (k == i)
+                    break;
+                arr[j] = arr[k];
+                j = k;
+            }
+            arr[j] = temp;
+        }
+    }
+}   
+
+
+    
