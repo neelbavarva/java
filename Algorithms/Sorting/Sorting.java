9253e78431de3d019b1a71142eec75f4749ffc25
neelbavarva <neelbavarva5@gmail.com>
2021-05-26 19:14:39 -0400

    tree

---
+++ Algorithms/Sorting/Sorting.java
@@ -0,0 +1,31 @@
+package Algorithms.Sorting;
+
+public class Sorting {
+
+    static public void bubbleSort(int arr[]) {
+        boolean isSorted;
+        for (int i = 0; i < arr.length; i++) {
+            isSorted = true;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int[] arr = { 4, 9, 1, 6, 2, 0 };
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
